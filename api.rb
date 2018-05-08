ce5097187daef02f62940b7fedf158e4eb02b893
Oleg Puzanov <puzanov@gmail.com>
2018-05-09 00:56:13 +0600

    added ticker feature

--- api.rb
+++ api.rb
@@ -16,3 +16,21 @@ get '/' do
     }
     result.to_json
 end
+
+get '/ticker' do
+    client = Cryptoexchange::Client.new
+    result = Hash.new
+    result['ticker'] = Array.new
+    pair = Cryptoexchange::Models::MarketPair.new(base: params[:base], target: params[:target], market: params[:market])
+    ticker = client.ticker(pair)
+    result['ticker'] << {
+        'last' => ticker.last,
+        'bid' => ticker.bid,
+        'ask' => ticker.ask,
+        'high' => ticker.high,
+        'low' => ticker.low,
+        'change' => ticker.change,
+        'volume' => ticker.volume
+    }
+    result.to_json
+end    
